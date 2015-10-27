b9aba7e72ec872b22e31d746530b0346c42a98d5
kevin <kevin.teal1@gmail.com>
2015-10-27 18:00:27 +0000

    ccs up

--- js_css/cloudy.js
+++ js_css/cloudy.js
@@ -8,3 +8,10 @@ function page_to(page){
 	console.log(page);
 	$( ":mobile-pagecontainer" ).pagecontainer( "change", "#"+page, { transition: "slide" } );
 }
+
+function getepsfor(){
+	console.log("getting shows");
+}
+function deleteshow(){
+	console.log("deleting show");
+}
